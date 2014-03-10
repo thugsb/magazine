90b89d226708c747cf7c92b62c5cf187df9bc125
Stu Basden <stu@tapiocacollective.com>
2014-03-10 11:43:48 -0400

    clone CURB website for structure

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>Center for the Urban River at Beczak - Sarah Lawrence College</title>
+  <meta name="description" content="">
+  <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1">
+  <script>if (screen.width < 320) {mvp = document.getElementById('testViewport').setAttribute('content','width=320');}</script>
+  
+  
+  <!--[if IE]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="core/css/slc.css">
+  <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+  <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7593272/684322/css/fonts.css" />
+  <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
+</head>
+<body>
+  <!--[if lt IE 7]>
+  <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+  <a class="skip-to-content" href="#content">Skip to Content</a>
+
+  <header class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#header-nav">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      
+      <div class="logo-wrap">
+        <a href="/" class="logo">
+          <img src="core/img/curb-logo.png" alt="Center for the Urban River logo"/>
+          <span>Sarah Lawrence College</span>
+          <em>Center for the Urban River at Beczak</em>
+        </a>
+      </div>
+    </div>
+    <nav class="collapse navbar-collapse" id="header-nav">
+      <a href="/"><i class="fa fa-home"></i></a>
+      <a href="/about">About the Center</a>
+      <a href="/programs">Programs</a>
+      <a href="/support">Support</a>
+      <a href="/river">The Hudson River</a>
+    </nav>
+  </header>
+  
+  
+  <div class="wrap">
+  
+    <section class="showcase">
+      <div class="cycle-slideshow" data-paused="true" data-slides="> .slide">
+        <div class="cycle-prev cycle-controls fa fa-chevron-left"></div>
+        <div class="cycle-next cycle-controls fa fa-chevron-right"></div>
+        <div class="slide">
+          <h1 class="sr-only">Center for the Urban River at Beczak</h1>
+          <img class="img-responsive" src="core/img/cover-river.jpg" alt="Urban River scene"/>
+        </div>
+        <div class="slide">
+          <i class="fa fa-question-circle slide-sturgeon" data-toggle="popover" title="The Giant Sturgeon" data-content="Veggies sunt bona vobis, proinde vos postulo esse magis pea soko brussels sprout broccoli soybean parsnip tomato parsley celery chard zucchini plantain eggplant maize chicory avocado."></i>
+          <i class="fa fa-question-circle slide-display" data-toggle="popover" title="A display" data-content="Spring onion cauliflower mustard prairie turnip onion broccoli rabe chicory gram celery winter purslane sorrel asparagus mung bean."></i>
+          <i class="fa fa-question-circle slide-table" data-toggle="popover" title="A table" data-content="Squash radicchio turnip chickweed collard greens chicory tigernut endive sorrel water spinach wattle seed."></i>
+          <i class="fa fa-question-circle slide-window" data-toggle="popover" title="Growing things" data-content="Garlic pea sprouts carrot wattle seed salad chicory melon beet greens tomato kohlrabi gram desert raisin celery black-eyed pea rock melon."></i>
+          <img class="img-responsive" src="core/img/cover-classroom.jpg" alt="Urban Center Classroom"/>
+        </div>
+        <div class="slide">
+          <img class="img-responsive" src="core/img/cover-park.jpg" alt="Urban Park scene"/>
+        </div>
+      </div>
+    </section>
+
+    <section id="content" class="hidden">
+
+    </section>
+  
+    <section class="navigate open">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3 data-toggle="collapse" data-target="#nav-participate">Participate</h3>
+          <div class="n-collapse collapse clearfix" id="nav-participate">
+            <h4>K-12 Programs</h4>
+            <p>Squash radicchio turnip chickweed collard greens chicory tigernut endive sorrel water spinach wattle seed.</p>
+            <h4>Community</h4>
+            <p>Lotus root kakadu plum courgette collard greens celtuce grape garbanzo bok choy pea sprouts.</p>
+            <h4>SLC Students</h4>
+            <p>Melon jícama desert raisin peanut scallion eggplant radish.</p>
+          </div>
+        </div>
+        <div class="col-sm-4 connect">
+          <h3 data-toggle="collapse" data-target="#nav-connect">Connect</h3>
+          <div class="n-collapse collapse clearfix" id="nav-connect">
+            <h4>Stay informed about what's happening at the Center</h4>
+            <div class="text-center">
+              <a href="#"><i class="fa fa-facebook-square"></i></a>
+              <a href="#"><i class="fa fa-twitter-square"></i></a>
+              <a href="#"><i class="fa fa-google-plus-square"></i></a>
+              <a href="#"><i class="fa fa-linkedin-square"></i></a>
+              <a href="#"><i class="fa fa-youtube"></i></a>
+            </div>
+            <h4>Join our mailing list</h4>
+            <form action="https://www.spectate.com/some-form" role="form">
+              <div class="row">
+                <div class="col-xs-6"><div class="form-group"><label class="sr-only">First name</label><input class="form-control" type="text" placeholder="First name" name="first_name"/></div></div>
+                <div class="col-xs-6"><div class="form-group"><label class="sr-only">Last name</label><input class="form-control" type="text" placeholder="Last name" name="last_name"/></div></div>
+              </div>
+              <div class="form-group"><label class="sr-only">Email</label><input class="form-control" type="email" placeholder="Email" name="email"/></div>
+              <button type="submit" class="btn btn-info btn-block">Continue <i class="fa fa-arrow-circle-right"></i></button>
+            </form>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <h3 data-toggle="collapse" data-target="#nav-support">Support</h3>
+          <div class="n-collapse collapse clearfix" id="nav-support">
+            <h4>Make an online gift to the Center</h4>
+            <p>Fast, secure, and tax-deductible. Make your gift today and make a difference.</p>
+            <h4>Corporate Sponsors and Partners</h4>
+            <p>Speak to our director about opportunities for companies to support the Center.</p>
+            <h5>Ryan Palmer</h5>
+            <p>Director, The Center for the Urban River<br/>
+              (914) 377-1900 x15</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+
+    <footer>
+      <div class="row">
+        <div class="col-sm-4">
+          <h5>The Center and Sarah Lawrence College</h5>
+          <p>Situated on the banks of the Hudson as a facility of Sarah Lawrence College, the Center for the Urban River at Beczak provides for a broad educational platform, establishes a Hudson River research program, and incorporates service learning and citizen science projects in the Yonkers community. Located in the Lawrence Park neighborhood adjoining Bronxville and Yonkers, SLC is a small, coeducational liberal arts college offering a unique, personalized education for undergraduate and graduate students as well as adult learners.</p>
+          <a href="#">Learn more about the Center for the Urban River</a>
+          <a href="http://www.slc.edu/" target="_blank">Learn more about Sarah Lawrence College</a>
+        </div>
+        <div class="col-sm-4">
+          <h5>Find us on Google Maps</h5>
+          <div class="responsiveIframe"><iframe width="350" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;q=sarah+lawrence+college+center+for+the+urban+river&amp;ie=UTF8&amp;hq=sarah+lawrence+college+center+for+the+urban+river&amp;hnear=&amp;t=p&amp;ll=40.938399,-73.901854&amp;spn=0.011347,0.01502&amp;z=15&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;q=sarah+lawrence+college+center+for+the+urban+river&amp;ie=UTF8&amp;hq=sarah+lawrence+college+center+for+the+urban+river&amp;hnear=&amp;t=p&amp;ll=40.938399,-73.901854&amp;spn=0.011347,0.01502&amp;z=15" style="color:#0000FF;text-align:left">View Larger Map</a></small></div>
+        </div>
+        <div class="col-sm-4">
+          <h5>Contact Info</h5>
+          <address>
+            Sarah Lawrence College Center for the Urban River at Beczak<br/><br/>
+            35 Alexander St.<br/>
+            Yonkers, NY 10701
+          </address>
+          <a class="phone" href="tel:+9143771900">Office: (914) 377-1900</a>
+        </div>
+      </div>
+      <div class="footer-slc"><img class="img-responsive center-block" src="core/img/slc-logo.png" alt="Sarah Lawrence College"/></div>
+    </footer>
+
+  </div><!-- .wrap -->
+  
+
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="/core/js/jquery.min.js"><\/script>')</script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+  <script src="core/js/jquery.cycle2.js"></script>
+  <script src="core/js/slc.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+  <script>
+  // (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+  //   function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+  //   e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+  //   e.src='//www.google-analytics.com/analytics.js';
+  //   r.parentNode.insertBefore(e,r)}(window,document,'script','ga')
+  // );
+  // ga('create','UA-XXXXX-X');ga('send','pageview');
+  </script>
+</body>
+</html>
