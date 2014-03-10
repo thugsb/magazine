5cb7e0cf3d4feef64583dbaad947dfdc89412f00
Stu Basden <stu@tapiocacollective.com>
2014-03-10 14:39:41 -0400

    initial mockup

---
+++ _header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>Magazine - Sarah Lawrence College</title>
+  <meta name="description" content="">
+  <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1">
+  <script>if (screen.width < 320) {mvp = document.getElementById('testViewport').setAttribute('content','width=320');}</script>
+
+
+  <!--[if IE]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="core/css/slc.css">
+  <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+  <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7593272/684322/css/fonts.css" />
+  <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
+</head>
+<body>
+  <!--[if lt IE 7]>
+  <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+  <a class="skip-to-content" href="#content">Skip to Content</a>
+
+  <header class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-mobile">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Sarah Lawrence Magazine</a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-mobile">
+      <form class="navbar-form navbar-left" role="search" action="/search">
+        <input type="hidden" name="scope" value="magazine">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Search</button>
+      </form>
+      <button type="button" class="pull-right btn btn-default toggle-menu" data-toggle="collapse" data-target="#navbar-menu">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="fa fa-align-justify"></span>
+      </button>
+      <div class="collapse" id="navbar-menu">
+        <ul>
+          <li><a href="#">Feature1</a></li>
+          <li><a href="#">Feature1</a></li>
+          <li><a href="#">Feature1</a></li>
+          <li><a href="#">Feature1</a></li>
+          <li><a href="#">Feature1</a></li>
+          <li><a href="/magazine/archives.html">Magazine Archives</a></li>
+          <li><a href="/">Sarah Lawrence Website</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
--- core/js/slc.js
+++ core/js/slc.js
@@ -1,11 +1,3 @@
 jQuery(function($) {
-  $('.slide .fa-question-circle').popover({placement:'bottom', container:'.showcase'});
   
-  $(window).scroll(function(e) {
-    if ($(window).scrollTop() > 100) {
-      $('.logo-wrap').addClass('off-top')
-    } else {
-      $('.logo-wrap').removeClass('off-top')
-    }
-  });
 });
