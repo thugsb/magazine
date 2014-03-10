c830f640406ea4052c5418bedee2a71764d51605
Stu Basden <stu@tapiocacollective.com>
2014-03-10 14:49:12 -0400

    add feature page and toc as panels

---
+++ feature.php
@@ -0,0 +1,18 @@
+<?php include('_header.php') ?>
+
+
+  <div class="wrap">
+    <section id="cover" class="cover">
+      <div class="cover-img feature1-img"></div>
+    </section>
+
+    <section id="content" class="content">
+      <h1>Trees and Stuff</h1>
+      <p>Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps? Every creative endeavor, fledgling business, or personal transformation is built on what came before. But some take the pieces—received ideas, failed experiments, raw materials— and transform them into something unmistakably new. Here we present five remarkable examples of invention and reinvention from the SLC community.</p>
+      <p>Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps? Every creative endeavor, fledgling business, or personal transformation is built on what came before. But some take the pieces—received ideas, failed experiments, raw materials— and transform them into something unmistakably new. Here we present five remarkable examples of invention and reinvention from the SLC community.</p>
+      <img src="media/img/starting_from_scratch_intro.png" alt="Starting From Scratch" class="img-responsive"/>
+      <p>Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps? Every creative endeavor, fledgling business, or personal transformation is built on what came before. But some take the pieces—received ideas, failed experiments, raw materials— and transform them into something unmistakably new. Here we present five remarkable examples of invention and reinvention from the SLC community.</p>
+    </section>
+  </div><!-- .wrap -->
+
+<?php include('_footer.php') ?>
--- index.php
+++ index.php
@@ -3,15 +3,93 @@
 
   <div class="wrap">
     <section id="cover" class="cover">
-      <div class="cover-img"></div>
+      <div class="cover-img mag-img"></div>
     </section>
 
     <section id="content" class="content">
       <h1>Starting From Scratch</h1>
-      <p>Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps? Every creative endeavor, fledgling business, or personal transformation is built on what came before. But some take the pieces—received ideas, failed experiments, raw materials— and transform them into something unmistakably new. Here we present five remarkable examples of invention and reinvention from the SLC community.</p>
-      <p>Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps? Every creative endeavor, fledgling business, or personal transformation is built on what came before. But some take the pieces—received ideas, failed experiments, raw materials— and transform them into something unmistakably new. Here we present five remarkable examples of invention and reinvention from the SLC community.</p>
-      <img src="media/img/starting_from_scratch_intro.png" alt="Starting From Scratch" class="img-responsive"/>
-      <p>Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps? Every creative endeavor, fledgling business, or personal transformation is built on what came before. But some take the pieces—received ideas, failed experiments, raw materials— and transform them into something unmistakably new. Here we present five remarkable examples of invention and reinvention from the SLC community.</p>
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title"><a href="feature.php">Feature</a></h3>
+            </div>
+            <div class="panel-body">
+              <a href="feature.php">Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps?</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Feature</h3>
+            </div>
+            <div class="panel-body">
+              Every creative endeavor, fledgling business, or personal transformation is built on what came before.
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Feature</h3>
+            </div>
+            <div class="panel-body">
+              Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps?
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Feature</h3>
+            </div>
+            <div class="panel-body">
+              Every creative endeavor, fledgling business, or personal transformation is built on what came before.
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Feature</h3>
+            </div>
+            <div class="panel-body">
+              Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps?
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Feature</h3>
+            </div>
+            <div class="panel-body">
+              Every creative endeavor, fledgling business, or personal transformation is built on what came before.
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Feature</h3>
+            </div>
+            <div class="panel-body">
+              Has anyone truly started from scratch, other than our cave-dwelling ancestors, perhaps?
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Feature</h3>
+            </div>
+            <div class="panel-body">
+              Every creative endeavor, fledgling business, or personal transformation is built on what came before.
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </div><!-- .wrap -->
 
